30c8f60c5459051fd1d894556acb59a086b57956
crehmann <crehmann@users.noreply.github.com>
2017-09-04 21:50:19 +0200

    Added binding project

---
+++ SampleApp/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    [Activity(Label = "SampleApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected MainActivity(System.IntPtr javaReference, Android.Runtime.JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public MainActivity() : base()
+        {
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            var button = FindViewById(Resource.Id.async_task);
+            button.Click += async (sender, e) => await Task.Factory.StartNew(() => Thread.Sleep(5000)).ConfigureAwait(false);
+        }
+    }
+}
+
---
+++ SampleApp/MainApplication.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Runtime;
+using Square.LeakCanary;
+using System;
+
+namespace SampleApp
+{
+    [Application]
+    internal class MainApplication : Application
+    {
+        private RefWatcher _refWatcher;
+
+        protected MainApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            SetupLeakCanary();
+        }
+
+        protected void SetupLeakCanary()
+        {
+            if (LeakCanaryXamarin.IsInAnalyzerProcess(this))
+            {
+                return;
+            }
+            _refWatcher = LeakCanaryXamarin.Install(this);
+        }
+    }
+}
